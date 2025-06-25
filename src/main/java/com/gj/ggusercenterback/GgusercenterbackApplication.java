c4d4e518fe59baada1d17dcbe3c5ca09e94318e5
Gj <2964820196@qq.com>
2025-06-25 16:08:20 +0800

    ggusercenter第一次提交，初始化后端，桀桀桀！

---
+++ src/main/java/com/gj/ggusercenterback/GgusercenterbackApplication.java
@@ -0,0 +1,13 @@
+package com.gj.ggusercenterback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgusercenterbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgusercenterbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gj/ggusercenterback/GgusercenterbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gj.ggusercenterback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgusercenterbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
